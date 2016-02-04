36e563068434c87c5d22bb861515e686857a9224
playboy王某某 <wm5821090>
2016-02-05 04:38:55 +0800

    fuck

--- main.go
+++ main.go
@@ -18,26 +18,14 @@ func init() {
 	beego.StaticDir["/ranbb/admin"] = "views"
 	beego.SessionGCMaxLifetime = 60000000
 	beego.SessionProvider = "mysql"
-	beego.SessionSavePath =  "cloudbridge:Cbcnspsp06@tcp(115.29.164.59:3306)/storedb?charset=utf8"
-//	beego.SessionDomain="http://127.0.0.1:8082/ranbb/admin/#/sessions/singin"
+	beego.SessionSavePath =  beego.AppConfig.String("mydb")
 
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
 	orm.Debug=true
 
-	orm.RegisterDataBase("default", "mysql", "cloudbridge:Cbcnspsp06@tcp(115.29.164.59:3306)/storedb?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("mydb"))
 }
 
 func main() {
-
-//	var FilterUser = func(ctx *context.Context) {
-//		_, ok := ctx.Input.Session("userInfo").(* model.Admin)
-//		if !ok && ctx.Request.RequestURI != "/ranbb/admin/uploadFile.html" {
-//			ctx.Redirect(301, "http://127.0.0.1:8082/ranbb/admin/uploadFile.html")
-//		}
-//	}
-//	beego.InsertFilter("/*",beego.BeforeRouter,FilterUser)
-
 	beego.Run()
-
-
 }
